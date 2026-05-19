97028a107c4f82a4e813e6fb58b1bcafa0ca4498
KrutalevichLev <krutalevichlev@gmail.com>
2026-05-19 17:00:42 +0300

    class "GamingRig"

--- PcClubOOPHomeworkProject/GamingRig.h
+++ PcClubOOPHomeworkProject/GamingRig.h
@@ -1,1 +1,24 @@
 #include "logic.h"
+
+class GamingRig {
+public:
+	int station_id;
+	int ram_gb;
+	int power_supply_unit;
+	double price_hour;
+	string cpu_model;
+	string gpu_model;
+
+	string toString() {
+		string msg = "BankAccount:\n";
+		msg += "ID: " + to_string(station_id) + "\n";
+		msg += "gb in ram: " + to_string(ram_gb) + "\n";
+		msg += "power supply unit: " + to_string(power_supply_unit) + "\n";
+		msg += "model of cpu: " + cpu_model + "\n";
+		msg += "model of gpu: " + gpu_model + "\n";
+		msg += "price per hour: " + to_string(price_hour);
+
+		return msg;
+	}
+
+};
